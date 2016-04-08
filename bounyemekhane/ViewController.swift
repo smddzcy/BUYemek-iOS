73673d79249b3326e47d86309fcaee389678a327
smddzcy <samedduzcay@gmail.com>
2016-04-08 15:32:49 +0300

    Initial Commit

---
+++ bounyemekhane/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bounyemekhane
+//
+//  Created by Samed Duzcay on 08/04/16.
+//  Copyright © 2016 Samed Duzcay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
